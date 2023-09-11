9349f880e89ef4b29b5dc6fd597277561da4e340
KAJI-JAPAN <kurowassan1206@gmail.com>
2023-09-12 00:24:26 +0900

    practise interface

--- InterfaceSample.java
+++ InterfaceSample.java
@@ -1,14 +1,25 @@
 import java.util.List;
 
+interface Named {
+    String name();
+}
+
 public class InterfaceSample {
-    record Student(String name, int score) {}
-    record Teacher(String name, String subject) {}
+    record Student(String name, int score) implements Named {}
+    record Teacher(String name, String subject) implements Named {}
 
     public static void main(String[] args) {
-        var people = List.of(new Student("kis", 80), new Teacher("hosoya", "Math"));
-        for(var p : people) {
-            var n = p instanceof Student s ? s.name() : p instanceof  Teacher t ? t.name() : "----"; 
+        var people = List.of(new Student("kis", 80), new Teacher("hosoya", "Math"), new Passenger());
+        for(Named p : people) {
+            var n = p.name();
             System.out.println("こんにちは%sさん".formatted(n));
+        }
+    }
+
+    static class Passenger implements Named {
+        @Override
+        public String name() {
+            return "通りすがり";
         }
     }
 }
